dbbcc66766c6a2d59273a04de79dda642a0389e5
Dirk Leffrang <dirk.leffrang@wiwi.uni-paderborn.de>
2016-03-22 12:01:08 +0100

    First Version

--- scripts.js
+++ scripts.js
@@ -1,13 +1,38 @@
 var introApp = angular.module('introApp', []);
 
 introApp.controller('IndexCtrl', function ($scope, $http) {
-    $scope.foo = "Hello World";
+    $scope.foo = "";
+    $scope.options = [];
+    $scope.form = {
+        option: null
+    };
+    $scope.success = false;
+    $scope.error = false;
 
-    // $http({
-    //     method: 'GET',
-    //     url: '/survey1.json'
-    // }).then(function(response) {
-    //     $scope.foo = response.data.name;
-    //     console.log(response);
-    // });
+    var foo = $http({
+        method: 'GET',
+        url: '/survey1.json'
+    });
+
+    foo.then(function(response) {
+        $scope.foo = response.data.name;
+        $scope.options = response.data.options;
+        console.log(response);
+    },function(response){
+        alert("Error");
+        console.log(response);
+    });
+
+    $scope.vote = function(form){
+        if(form.option !== null){
+            $http.get('/vote.json',form).then(function(response) {
+                $scope.success = true;
+                $scope.error = false;
+            });
+        } else{
+            $scope.error = true;
+        }
+    };
+
+
 });
